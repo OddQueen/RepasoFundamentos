465d39daf5bc12499fd51007de9ae63b65cf7e2f
OddQueen <queensleymei@gmail.com>
2024-04-17 00:08:00 +0200

    point

---
+++ algebraProject/pointTest.ts
@@ -0,0 +1,14 @@
+import { Point } from "./point";
+
+let myPoint = new Point(3, 5);
+
+console.log(myPoint);
+
+console.log(myPoint.getX());
+ myPoint.setX(10);
+
+console.log(myPoint.getY());
+myPoint.setY(8);
+
+console.log(myPoint.toString());
+myPoint.toString();
